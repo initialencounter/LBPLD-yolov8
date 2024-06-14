0875c7f16dfd93b54a52869115146a3ba6640c25
initialencounter <2911583893@qq.com>
2024-06-14 22:55:24 +0800

    。

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
-from ultralytics import YOLO
+from ultralytics import YOLO # type: ignore
 
 # Load a model
-model = YOLO(r"C:\Users\29115\yolov8\ultralytics\runs\detect\train2\weights\best.pt")  # load an official model
+model = YOLO(r"C:\Users\29115\yolov8\ultralytics\runs\detect\train4\weights\best.pt")  # load an official model
 
 # Predict with the model
 if __name__ == '__main__':
